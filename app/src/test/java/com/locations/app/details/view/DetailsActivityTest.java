4694137357ec9a7fe9b6f8cb4d61d06eb61c6bef
kseth2 <gh_kunal2>
2017-07-03 08:19:03 -0500

    Adding test dependencies, clean up and comments

--- app/src/test/java/com/locations/app/details/view/DetailsActivityTest.java
+++ app/src/test/java/com/locations/app/details/view/DetailsActivityTest.java
@@ -1,16 +1,63 @@
 package com.locations.app.details.view;
 
-import org.junit.Test;
+import android.content.Intent;
 
+import com.locations.app.BuildConfig;
+import com.locations.app.details.presenter.DetailsPresenter;
+import com.locations.app.home.view.HomeActivity;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.android.controller.ActivityController;
+import org.robolectric.annotation.Config;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+@Ignore
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class)
 public class DetailsActivityTest {
 
-    @Test
-    public void onCreate() throws Exception {
+    private ActivityController<DetailsActivity> activityController;
+    private Intent intent = new Intent();
+
+    @Before
+    public void setup() {
 
     }
 
     @Test
+    public void onCreate_throwsException_forInvalidId() throws Exception {
+        intent.putExtra(HomeActivity.ID, "1L");
+        activityController = Robolectric.buildActivity(DetailsActivity.class, intent);
+        try {
+            activityController.create().get();
+        } catch (Exception exception) {
+            assertThat(exception.getClass()).isInstanceOf(RuntimeException.class);
+        }
+    }
+
+    @Test
+    public void onCreate_withValidId() throws Exception {
+        intent.putExtra(HomeActivity.ID, 1L);
+        activityController = Robolectric.buildActivity(DetailsActivity.class, intent);
+
+        activityController.create().start().resume().get();
+
+        DetailsPresenter mockDetailsPresenter = mock(DetailsPresenter.class);
+
+        verify(mockDetailsPresenter).loadLocation(1L);
+    }
+
+    @Test
     public void onLoadLocationSuccess() throws Exception {
+
 
     }
 
@@ -26,7 +73,13 @@ public class DetailsActivityTest {
 
     @Test
     public void onMapReady() throws Exception {
-
+//        GoogleMapUtility mockGoogleMapUtility = mock(GoogleMapUtility.class);
+//        GoogleMap googleMap = activity.getMap;
+//
+//        activity.onMapReady(googleMap);
+//
+//        verify(mockGoogleMapUtility).addMarker(any(LatLng.class), anyString());
+//        verify(mockGoogleMapUtility).moveCamera(any(LatLng.class), anyFloat());
     }
 
 }
